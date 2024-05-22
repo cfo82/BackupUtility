936b08badbaf8a01ffc6ac73d2dbeb7aaa43377d
Christian Oberholzer <christian@oberholzer.me>
2024-05-23 01:30:19 +0200

    Produce installer during build.

---
+++ BackupUtility.Installer/ExcludeFiles.xslt
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet
+        xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+        xmlns:wix="http://wixtoolset.org/schemas/v4/wxs"
+        xmlns="http://schemas.microsoft.com/wix/2006/wi"
+        version="1.0"
+        exclude-result-prefixes="xsl wix">
+
+    <xsl:output method="xml" indent="yes" omit-xml-declaration="yes"/>
+
+    <xsl:strip-space elements="*"/>
+
+    <xsl:key name="ExeToRemove" match="wix:Component[ substring( wix:File/@Source, string-length( wix:File/@Source ) - 3 ) = '.exe' ]" use="@Id"/>
+    <xsl:key name="PdbToRemove" match="wix:Component[ substring( wix:File/@Source, string-length( wix:File/@Source ) - 3 ) = '.pdb' ]" use="@Id" />
+
+    <xsl:template match="@*|node()">
+        <xsl:copy>
+            <xsl:apply-templates select="@*|node()"/>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="*[ self::wix:Component or self::wix:ComponentRef ][ key( 'ExeToRemove', @Id ) ]"/>
+    <xsl:template match="*[ self::wix:Component or self::wix:ComponentRef ][ key( 'PdbToRemove', @Id ) ]"/>
+
+</xsl:stylesheet>
--- Build/Build.cs
+++ Build/Build.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
@@ -5,11 +6,12 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
 using Serilog;
 
 class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.Test);
+    public static int Main() => Execute<Build>(x => x.BuildInstaller);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -22,8 +24,18 @@ class Build : NukeBuild
 
     GitHubActions? GitHubActions => GitHubActions.Instance;
 
+    Project? InstallerProject => Solution?.Projects.FirstOrDefault(p => p.Name == "BackupUtility.Installer");
+
+    AbsolutePath BuildDirectory => RootDirectory / "Build";
+
     [Parameter]
     AbsolutePath TestResultDirectory = RootDirectory + "/.nuke/Artifacts/Test-Results/";
+
+    [Parameter]
+    AbsolutePath PublishDirectory = RootDirectory + "/.nuke/Artifacts/Publish/";
+
+    [Parameter]
+    AbsolutePath InstallerDirectory = RootDirectory + "/.nuke/Artifacts/Installer/";
 
     Target LogInformation => _ => _
         .Executes(() =>
@@ -60,8 +72,31 @@ class Build : NukeBuild
         .DependsOn(LogInformation)
         .Executes(() =>
         {
-            DotNetTasks.DotNetClean();
+            InstallerDirectory.CreateOrCleanDirectory();
             TestResultDirectory.CreateOrCleanDirectory();
+            PublishDirectory.CreateOrCleanDirectory();
+
+            Log.Information($"Build dir {BuildDirectory}");
+
+            RootDirectory.GlobDirectories("**/bin")
+                .Where(d => !d.ToString().StartsWith(BuildDirectory))
+                .ForEach(d =>
+                {
+                    Log.Information($"Delete directory {d}");
+                    d.DeleteDirectory();
+                });
+            RootDirectory.GlobDirectories("**/obj")
+                .Where(d => !d.ToString().StartsWith(BuildDirectory))
+                .ForEach(d =>
+                {
+                    Log.Information($"Delete directory {d}");
+                    d.DeleteDirectory();
+                });
+
+            DotNetTasks.DotNetClean(_ => _
+                .SetProject(Solution));
+            DotNetTasks.DotNetClean(_ => _
+                .SetProject(InstallerProject));
         });
 
     Target Restore => _ => _
@@ -69,7 +104,8 @@ class Build : NukeBuild
         .Executes(() =>
         {
             DotNetTasks.DotNetRestore(_ => _
-                .SetProjectFile(Solution));
+                .SetProjectFile(Solution)
+                .SetRuntime("win-x64"));
         });
 
     Target Compile => _ => _
@@ -94,5 +130,56 @@ class Build : NukeBuild
                 .SetNoBuild(true)
                 .SetResultsDirectory(TestResultDirectory)
                 .EnableCollectCoverage());
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Test)
+        .OnlyWhenDynamic(() => Configuration == Configuration.Release)
+        .Executes(() =>
+        {
+            var wpfProject = Solution?.Projects.FirstOrDefault(p => p.Name == "BackupUtility.Wpf");
+            Assert.NotNull(wpfProject);
+
+            DotNetTasks.DotNetPublish(_ => _
+                .SetProject(wpfProject)
+                .SetNoRestore(true)
+                .SetNoBuild(true)
+                .SetConfiguration(Configuration)
+                .SetOutput(PublishDirectory)
+                .SetSelfContained(false)
+                .SetRuntime("win-x64")
+                .SetVersion(GitVersion?.AssemblySemVer)
+                .SetAssemblyVersion(GitVersion?.AssemblySemVer)
+                .SetPublishTrimmed(false)
+                .SetPublishReadyToRun(false));
+        });
+
+    Target BuildInstaller => _ => _
+        .DependsOn(Publish)
+        .Produces(RootDirectory / "BackupUtility.Installer" / "bin" / "x64" / "Release")
+        .OnlyWhenDynamic(() => Configuration == Configuration.Release)
+        .Executes(() =>
+        {
+            Assert.NotNull(Solution);
+            Assert.NotNull(InstallerProject);
+
+            DotNetTasks.DotNetRestore(_ => _
+                .SetProjectFile(InstallerProject)
+                .SetRuntime("win-x64"));
+
+            DotNetTasks.DotNetBuild(_ => _
+                .SetNoRestore(true)
+                .SetProjectFile(InstallerProject)
+                .SetConfiguration(Configuration)
+                .SetVersion(GitVersion?.AssemblySemVer)
+                .SetAssemblyVersion(GitVersion?.AssemblySemVer)
+                .SetProperty("SolutionDir", Solution?.Path.Parent));
+
+            var installerBinDir = RootDirectory / "BackupUtility.Installer" / "bin" / Configuration / "en-US";
+
+            Log.Information($"Copy installer files from {installerBinDir} to {InstallerDirectory}");
+
+            System.IO.Directory.GetFiles(installerBinDir)
+                .ForEach(f => System.IO.File.Copy(f, System.IO.Path.Combine(InstallerDirectory, System.IO.Path.GetFileName(f))));
         });
 }
